2410ef201f3f4d623f1d248e681a45bf328f0cfb
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-06-19 15:42:16 +0900

    android_demo

---
+++ android/app/src/main/java/com/example/myfirstapp/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.myfirstapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if(action.equals("android.intent.action.BOOT_COMPLETED")){
+            Intent i = new Intent(context, MainActivity.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+    }
+}
