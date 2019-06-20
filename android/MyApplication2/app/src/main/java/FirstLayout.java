9bc61cef6667b8a60a7b25641c28e9f0f2e477cf
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-06-20 15:11:35 +0900

    two more projects..now im done

---
+++ android/MyApplication2/app/src/main/java/FirstLayout.java
@@ -0,0 +1,19 @@
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+
+public class FirstLayout extends Fragment {
+    View v;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.first_layout, container, false);
+    }
+}
+
